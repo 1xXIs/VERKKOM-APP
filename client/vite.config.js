d9ed98b3ec30c2955f3b34abffcf5bc096a3cd51
Alexis <alexispedraza600@gmail.com>
2026-02-02 17:46:26 -0600

    Fix: Add postcss config and clean vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,12 +6,5 @@ import autoprefixer from 'autoprefixer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer(),
-      ],
-    },
-  },
+
 })
