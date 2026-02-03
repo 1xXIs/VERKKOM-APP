fdd3ab77fb9018cafea15b04e760b4d65b5b2381
Alexis <alexispedraza600@gmail.com>
2026-02-02 21:29:47 -0600

    Debug: Switch Logger to Console for Vercel Visibility

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,14 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
-const logFile = path.join(__dirname, '../../debug.log');
-
 const logToFile = (message, data) => {
     const timestamp = new Date().toISOString();
-    const content = `\n[${timestamp}] ${message}\n${JSON.stringify(data, null, 2)}\n`;
-    fs.appendFile(logFile, content, (err) => {
-        if (err) console.error("Failed to write to log file:", err);
-    });
+    console.log(`[${timestamp}] [DEBUG] ${message}`, JSON.stringify(data, null, 2));
 };
 
 module.exports = logToFile;
